686d3f55f47d0e1c610523f4b67afd959818183a
Steven Stanek <steven@shopkick.com>
2013-05-28 00:46:39 -0700

    Add and cleanup tests

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <unistd.h>
+
 #include "disk_cache.h"
 
 
@@ -8,9 +10,10 @@ int createTest() {
   DCCache cache = DCMake("/tmp", 16);
   DCCloseAndFree(cache);
   printf("createTest Complete");
+  return 0;
 }
 
-int addTest() {
+int simpleAddTest() {
   char *test_key = "TEST KEY";
   char *test_val = "TEST VAL";
 
@@ -18,24 +21,57 @@ int addTest() {
   DCCloseAndFree(cache);
 
   DCCache cache2 = DCLoad("/tmp");
-  DCAdd(cache2, test_key, test_val, strlen(test_val) + 1); //+1 to capture the \0
+  DCAdd(cache2, test_key, (uint8_t *)test_val, strlen(test_val) + 1); //+1 to capture the \0
   DCCloseAndFree(cache2);
 
   DCCache cache3 = DCLoad("/tmp");
   DCData result = DCLookup(cache3, test_key);
   DCCloseAndFree(cache3);
 
-  if (strncmp(result->data, test_val, strlen(test_val)) == 0) {
+  if (strncmp((char *)result->data, test_val, strlen(test_val)) == 0) {
     printf("addTest passed '%s' == '%s'\n", test_val, result->data);
     return 0;
   } else {
     printf("addTest failed '%s' != '%s'\n", test_val, result->data);
-    return 0;
+    return 1;
   }
+}
+
+int addTestWithOverwrites() {
+  DCCache cache = DCMake("/tmp", 2);
+  // It should evict the oldest
+  DCAdd(cache, "key1", (uint8_t *)"val1", 5);
+  usleep(2000);
+  DCAdd(cache, "key2", (uint8_t *)"val2", 5);
+  usleep(2000);
+  DCAdd(cache, "key3", (uint8_t *) "val3", 5);
+
+  DCData r1 = DCLookup(cache, "key1");
+  DCData r2 = DCLookup(cache, "key2");
+  DCData r3 = DCLookup(cache, "key3");
+
+  if (r1) {
+    printf("Test Failure: Should have deleted 'key1'\n");
+    return 1;
+  }
+  if (strcmp((char *) r2->data, "val2") != 0) {
+    printf("Test Failure: Should have found 'val2' as value for 'key2', instead got '%s'\n",
+           (char *) r2->data);
+    return 1;
+  }
+  if (strcmp((char *) r3->data, "val3") != 0) {
+    printf("Test Failure: Should have found 'val3' as value for 'key3', instead got '%s'\n",
+           (char *) r3->data);
+    return 1;
+  }
+
+  printf("Passed addTestWithOverwrites\n");
+  return 0;
 }
 
 int main(int argc, char **argv) {
   createTest();
-  addTest();
+  simpleAddTest();
+  addTestWithOverwrites();
   return 0;
 }
