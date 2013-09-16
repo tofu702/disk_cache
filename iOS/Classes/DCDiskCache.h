cabf266953e1a604cacb9c3d80bb56ee324d5054
Steven Stanek <steven@shopkick.com>
2013-09-16 03:25:50 -0700

    Initial (incomplete version of iOS library)

--- iOS/Classes/DCDiskCache.h
+++ iOS/Classes/DCDiskCache.h
@@ -6,8 +6,52 @@
 //  Copyright (c) 2013 stevenstanek. All rights reserved.
 //
 
+/**
+ * This file specifies the interface for a DCDiskCache. A DCDiskCache is built by storing cache entries
+ * to disk as individual files and with a memory mapped file for book keeping. This file is a wrapper
+ * around the C library and it should be extremely fast.
+ * A few notes:
+ * 1. The cache is implemented as set associative cache, meaning that a given key has several candidate positions
+ *    and a new insert will automatically replace the worst entry at those candidate positions.
+ * 2. The cache has a maximum size, when this size is hit, a substantial number of entries will be evicted
+ *    in order to avoid doing the reasonably expensive eviction operation on every subsequent insert.
+ */
+
 #import <Foundation/Foundation.h>
 
+FOUNDATION_EXPORT const NSUInteger DC_NO_CACHE_LIMIT_MAX_BYTES;
+
 @interface DCDiskCache : NSObject
+
+/**
+ * Load a previously stored cache from the provided path, if one does not exist, create it and return it
+ */
++ (id)loadCacheWithPath:(NSString *)cachePath;
+
+
+/**
+ * Designated Initializer
+ * cachePath: A valid path to a directory that the disk cache should use, this directory should be empty
+ * numLines: The number of lines (the maximum number of entries) in the cache
+ * maxBytes: The maximum total size of the cache, specify DC_NO_CACHE_LIMIT_MAX_BYTES
+ */
+- (id)initWithPath:(NSString *)cachePath numLines:(NSUInteger)numLines maxBytes:(NSUInteger) maxBytes;
+
+
+
+/**
+ *
+ */
+- (void)setItem:(id<NSCoding>)item forKey:(NSString*)key;
+
+/**
+ *
+ */
+- (id)itemForKey:(NSString *)key;
+
+/**
+ * NOT YET IMPLEMENTED
+ */
+- (void)removeItemForKey:(NSString *)key;
 
 @end
--- iOS/Classes/DCDiskCache.m
+++ iOS/Classes/DCDiskCache.m
@@ -7,7 +7,62 @@
 //
 
 #import "DCDiskCache.h"
+#import "disk_cache.h"
+
+const NSUInteger DC_NO_CACHE_LIMIT_MAX_BYTES = 0;
+
+@interface DCDiskCache ()
+
+@property DCCache cache;
+
+
+@end
 
 @implementation DCDiskCache
+
+
+
++ (id)loadCacheWithPath:(NSString *)cachePath {
+  
+}
+
+- (id)initWithPath:(NSString *)cachePath numLines:(NSUInteger)numLines maxBytes:(NSUInteger) maxBytes {
+  self = [super init];
+  if (self) {
+    self.cache = DCMake((char*)[cachePath cStringUsingEncoding:NSASCIIStringEncoding],
+                        (uint32_t)numLines,
+                        (uint64_t)maxBytes);
+  }
+  return self;
+}
+
+- (void)dealloc {
+  DCCloseAndFree(self.cache);
+}
+
+- (void)setItem:(id<NSCoding>)item forKey:(NSString*)key {
+  NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:item];
+  DCAdd(self.cache,
+        (char *)[key cStringUsingEncoding:NSASCIIStringEncoding],
+        (uint8_t *) [archivedData bytes],
+        (uint64_t)[archivedData length]);
+  
+}
+
+- (id)itemForKey:(NSString *)key {
+  DCData foundEntry = DCLookup(self.cache, (char *)[key cStringUsingEncoding:NSASCIIStringEncoding]);
+  if (!foundEntry) {
+    return nil;
+  }
+  
+  NSData *data = [NSData dataWithBytes:foundEntry->data length:foundEntry->data_len];
+  return [NSKeyedUnarchiver unarchiveObjectWithData:data];
+}
+
+
+- (void)removeItemForKey:(NSString *)key {
+    //TODO: Implement
+}
+
 
 @end
